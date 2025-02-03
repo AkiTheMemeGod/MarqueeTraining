2d8a882015b20e5d3dd559031807cf71839c9644
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-02-03 15:22:05 +0530

    Implementation of DoublyLinkedList.py

---
+++ DAY 1/DoublyLinkedList.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, data):
+        self.prev = None
+        self.data = data
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        new = Node(data)
+        if self.head:
+            new.next = self.head
+            new.next.prev = new
+            self.head = new
+        else:
+            self.head = new
+            new.next = None
+
+        new.prev = None
+
+    def insert_at_end(self, data):
+        new = Node(data)
+        if self.head:
+            ptr = self.head
+            while ptr.next:
+                ptr = ptr.next
+            ptr.next = new
+            new.prev = ptr
+            new.next = None
+        else:
+            self.head = new
+            new.prev = None
+            new.next = None
+
+    def __str__(self):
+        ptr = self.head
+        while ptr:
+            print(f"|{str(ptr.data)}|", end='<->')
+
+            ptr = ptr.next
+        return "End"
+
+dll = DoublyLinkedList()
+
+dll.insert_at_end(4)
+dll.insert_at_end(3)
+dll.insert_at_end(2)
+dll.insert_at_end(1)
+
+print(dll)
