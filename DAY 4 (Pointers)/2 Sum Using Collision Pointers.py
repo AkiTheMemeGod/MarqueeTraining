43dd2428c0ec2c222cde6f4533c4fc10311da722
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-02-06 11:29:53 +0530

    Add implementation for 2 Sum using Collision Pointers

---
+++ DAY 4 (Pointers)/2 Sum Using Collision Pointers.py
@@ -0,0 +1,40 @@
+"""class Node:
+    def __init__(self, data):
+        self.next = None
+        self.data = data
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def convert_list_to_ll"""
+
+nums = list(map(int,input("Enter Elements : ").split(' ')))
+target = int(input("Enter the target sum pair : "))
+print(nums)
+"""
+f = 0
+r = -1
+i = 0
+
+while i < len(nums):
+    if nums[f] + nums[r] == target:
+        print(f"({nums[f]}, {nums[r]})")
+        break
+    f += 1
+    r -= 1
+    i += 1"""
+
+
+left, right = 0 , len(nums)-1
+while left < right:
+    cur = nums[left] + nums[right]
+    if cur == target:
+        print(True)
+        exit()
+    elif cur > target:
+        right -= 1
+    else:
+         left += 1
+
+print(False)
