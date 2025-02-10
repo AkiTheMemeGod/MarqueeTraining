d7fa9a76d22a95a0da77e9a685fb13caf7e4ca32
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-02-10 10:31:48 +0530

    Refactor sliding window implementation for maximum sum subarray

--- DAY 4 (Pointers)/Sliding Window.py
+++ DAY 4 (Pointers)/Sliding Window.py
@@ -3,9 +3,25 @@ nums = list(map(int, input("Enter the elements of the array : ").split()))
 sub = int(input("Window Size : "))
 
 sums = []
+"""
+-------------------------------------------
+My Slicing Implementation
+-------------------------------------------
+
 
 for i in range(sub):
     if i == len(nums) - sub:
         break
     sums.append(sum(nums[i:sub+i]))
-print("Maximum Sum Sub Array : ",max(sums))
+print("Maximum Sum Sub Array : ",max(sums))"""
+
+
+i = 0
+cur = sum(nums[:sub])
+maxi = cur
+while sub < len(nums):
+    cur = cur - nums[i] + nums[sub]
+    i += 1
+    sub += 1
+    maxi = max(cur, maxi)
+print(maxi)
