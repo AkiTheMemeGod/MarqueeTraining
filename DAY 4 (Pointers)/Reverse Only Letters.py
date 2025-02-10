0d3f8233ac5f8df23b51204a639ff412ad88cdd8
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-02-10 10:31:41 +0530

    Add implementation to reverse only letters in a string

---
+++ DAY 4 (Pointers)/Reverse Only Letters.py
@@ -0,0 +1,19 @@
+x = input("Enter a String : ")
+# x = list(x)
+f = 0
+r = len(x) - 1
+
+for _ in x:
+    if f > r:
+        break
+    if x[f].isalpha() and x[r].isalpha():
+        x[f], x[r] = x[r], x[f]
+        f += 1
+        r -= 1
+    elif x[f].isalpha():
+        r -= 1
+    elif x[r].isalpha():
+        f += 1
+x = "".join(x)
+print(x)
+
