76b29cf96c890e8e963cb4cab4d8798df73e3e9f
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-02-10 10:31:23 +0530

    Add implementation to find the length of the longest substring without repeating characters

---
+++ DAY 4 (Pointers)/Longest SubString without repeating characters.py
@@ -0,0 +1,18 @@
+
+nums = input("Enter a buzz Word : ")
+sub = 0
+i = 0
+cur = ""
+sets = []
+while sub <= len(nums):
+    cur = nums[i:sub]
+    if len(set(cur)) == len(cur):
+        sub += 1
+        sets.append(cur)
+        continue
+    i += 1
+    sub = 0
+
+o = [len(set(i)) for i in sets]
+
+print(max(o))
