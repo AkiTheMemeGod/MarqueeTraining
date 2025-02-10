91f20ee88461144e54c8bfe124601be1d496ef00
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-02-10 10:33:03 +0530

    Add initial implementations of post-increment and pre-increment examples in C

---
+++ PsuedoCodes/post_inc_pre_inc.c
@@ -0,0 +1,9 @@
+
+int main(){
+    #include<stdio.h>
+    int i = 10;
+    i = i++ + ++i;
+    printf("%d", i);
+    return 0;
+
+}
---
+++ PsuedoCodes/pre_inc_post_inc.c
@@ -0,0 +1,7 @@
+int main(){
+    #include<stdio.h>
+    int i = 10;
+    printf("%d %d\n", i+=20, i-=20);
+    return 0;
+
+}
