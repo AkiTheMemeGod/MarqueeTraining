8039e27f581afa2786d961ab92d4e4d8e4cb9417
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-02-12 10:20:57 +0530

    Add DFS implementation using stacks in Python

---
+++ DAY 7 Graphs/DFS using Stacks.py
@@ -0,0 +1,27 @@
+graph = {
+    0:[1,2,3],
+    1:[0, 4, 5],
+    2:[0, 6],
+    3:[0,7],
+    4:[1],
+    5:[1],
+    6:[2],
+    7:[3]
+}
+start = 0
+visited = set()
+stack = [start]
+visited.add(start)
+
+while stack:
+    # print(stack)
+
+    node = stack.pop()
+
+    print(node, end= " ")
+
+    for i in graph[node][::-1]:
+        if i not in visited:
+            visited.add(i)
+            stack.append(i)
+
