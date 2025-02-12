26dad03b409e89e8c86aa97047adce009711fa11
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-02-12 10:20:47 +0530

    Add BFS implementation using queues in Python

---
+++ DAY 7 Graphs/BFS using Queues.py
@@ -0,0 +1,22 @@
+graph = {
+    0:[1,2,3],
+    1:[0, 4, 5],
+    2:[0, 6],
+    3:[0,7],
+    4:[1],
+    5:[1],
+    6:[2],
+    7:[3]
+}
+start = 0
+visited = set()
+queue = [start]
+visited.add(start)
+
+while queue:
+    node = queue.pop(0)
+    print(node, end= " ")
+    for i in graph[node]:
+        if i not in visited:
+            visited.add(i)
+            queue.append(i)
